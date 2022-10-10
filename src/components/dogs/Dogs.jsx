cee91858b748f53cf277fc6976f0d4eec427c14b
Poison512 <adrian512@Adrians-MacBook-Pro.local>
2022-10-10 13:36:59 -0500

    creating login page but will come back to it

---
+++ src/components/dogs/Dogs.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './dogs.css'
+
+
+const data = [
+  {
+    id:1 ,
+    // image: IMG1,
+    title: 'PUPPIES '
+  },
+  {
+    id:2 ,
+    // image: IMG1,
+    title: 'ADULT DOGS'
+  },
+]
+const Dogs = () => {
+  return (
+    <section id="dogs">
+      <h5>DOGS</h5>
+      <h2>DOGS</h2>
+
+      <div className='container dogs-container'>
+        {
+          data.map(({ id, image,title}) => {
+            return (
+              <article key={id} className='dogs-item'>
+          <div className='dogs-item-image'></div>
+          <img src={image}/>
+          <h3>{title}</h3>
+
+        </article>
+            )
+          })
+        }
+
+      </div>
+      </section>
+  )
+}
+export default Dogs
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+// import Login from '../../src/Login'
+
 const Header = () => {
   return (
     <header>
       <section>
       <div className="container header_container">
+
         <h5>WELCOME TO </h5>
         <h1> HAPPY PAWS</h1>
         <CTA/>
+        {/* <Login/> */}
         
         </div>
         </section>
