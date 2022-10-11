e321d4536d7fde253c4c8a91d390d77240541e42
garcia2697 <luisgarcia.lg37@gmail.com>
2022-10-11 02:24:20 -0500

    adds a back to top button

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href="#dogs" className='btn' >VIEW DOGS</a>
-        <a href="#cats" className='btn btn-primary'> VIEW CATS</a>
-        </div>
+      <a href="#dogs" className='btn' >VIEW DOGS</a>
+      <a href="#cats" className='btn btn-primary'> VIEW CATS</a>
+    </div>
   )
 }
 
