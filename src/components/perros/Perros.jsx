8cf0e7a1d7e874962729174f7dc02ed6842f0921
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-10-11 23:28:28 -0500

    Cards.

--- src/components/perros/Perros.jsx
+++ src/components/perros/Perros.jsx
@@ -21,49 +21,49 @@ function Perros() {
 
   return (
     <article className='cats-item flex grid grid-cols-3'>
-      <div class="max-w-sm p-4 rounded overflow-hidden shadow-lg">
-        <img class="w-full scale-80" src= {Dog1} alt="" id="perro-img" />
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">Doggo aka</div>
-            <h3 class="text-gray-700 text-base">Husky</h3>
-            <h3 class="text-gray-700 text-base">Age: 1 year</h3>
+      <div className="max-w-sm p-4 rounded overflow-hidden shadow-lg">
+        <img className="w-full object-contain" src= {Dog1} alt="" id="perro-img" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Doggo aka</div>
+            <h3 className="text-gray-700 text-base">Husky</h3>
+            <h3 className="text-gray-700 text-base">Age: 1 year</h3>
           </div>
-          <div class="px-6 pt-4 pb-2">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#pupper</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#husky</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bark512</span>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#pupper</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#husky</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bark512</span>
           </div>
         </div>
 
-        <div class="max-w-sm p-4 rounded overflow-hidden shadow-lg">
-          <img class="w-full scale-150 p-10" src= {Dog2} alt="" id="perro-img" />
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Doggo</div>
-              <h3 class="text-gray-700 text-base">Husky</h3>
-              <h3 class="text-gray-700 text-base">Age: 1 year</h3>
+        <div className="max-w-sm p-4 rounded overflow-hidden shadow-lg">
+          <img className="w-full object-contain" src= {Dog2} alt="" id="perro-img" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Doggo</div>
+              <h3 className="text-gray-700 text-base">Husky</h3>
+              <h3 className="text-gray-700 text-base">Age: 1 year</h3>
             </div>
-            <div class="px-6 pt-4 pb-2">
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#pupper</span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#husky</span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bark512</span>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#pupper</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#husky</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bark512</span>
             </div>
           </div>
 
-          <div class="max-w-sm p-4 rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="husky-img.jpeg" alt="" id="perro-img" />
-            <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">Doggo</div>
-                <h3 class="text-gray-700 text-base">Husky</h3>
-                <h3 class="text-gray-700 text-base">Age: 1 year</h3>
+          <div className="max-w-sm p-4 rounded overflow-hidden shadow-lg">
+            <img className="w-full" src="husky-img.jpeg" alt="" id="perro-img" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Doggo</div>
+                <h3 className="text-gray-700 text-base">Husky</h3>
+                <h3 className="text-gray-700 text-base">Age: 1 year</h3>
               </div>
-              <div class="px-6 pt-4 pb-2">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#pupper</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#husky</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bark512</span>
+              <div className="px-6 pt-4 pb-2">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#pupper</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#husky</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bark512</span>
               </div>
             </div>
 
-            <div class="max-w-sm p-4 rounded overflow-hidden shadow-lg">
+            <div className="max-w-sm p-4 rounded overflow-hidden shadow-lg">
         <img class="w-full" src="husky-img.jpeg" alt="" id="perro-img" />
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">Doggo</div>
