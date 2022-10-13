d69f4bf6568673ada184f149bd1493e6d0d0d443
smillkaa <104727784+smillkaa@users.noreply.github.com>
2022-10-12 20:35:26 -0700

    database

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts',
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
   }
-);
+)
+.then(console.log("Database Connection Successful!"))
+.catch(err => {
+  console.log(err)
+})
 
 module.exports = mongoose.connection;
--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -1,11 +1,20 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs ("queryName is gonna be how we name the query for what we are looking for in the database, and 'string' is the datatype that will be returned")
+// create our typeDefs ("type User is how we customly name the query for what we are looking for in the database, and 'String' or 'Int' is the datatype that will be returned")
 const typeDefs = gql`
-type Query {
-    queryName: String 
-}
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+    }
 `;
 
 // export the typeDefs
